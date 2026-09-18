152b4ce06efc5313ba30ab33eea2d2016042d557
jsavilan <js.avilan@uniandes.edu.co>
2026-09-18 10:20:09 -0500

    Add activity screens and update navigation routes
    - Implement ActivityDetailScreen, ActivityCreatedScreen, and CreateActivityPlaceholderScreen.
    Yet onlyu Activity is fully implemented, the rest have placeholder text just like the profile screen
    - Update AppRoutes for new screens.
    - Modify ActivitiesScreen to include navigation to activity detail.
    - Adjust DashboardScreen to utilize updated ActivitiesScreen.

---
+++ lib/screens/activity_detail_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ActivityDetailScreen extends StatelessWidget {
+  const ActivityDetailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SafeArea(
+      child: Center(
+        child: Text('ActivityDetail Placeholder', style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700)),
+      ),
+    );
+  }
+}
